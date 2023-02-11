9ca43b5d7384eac11c7f622581152bf76edf05cb
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2023-02-11 12:10:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>FishTrade</h1>
+      <h2>this app will let u:<br />
+      <h3>  - trade with guis.<br />
+        -trade with clis.<br />
+        -connect to all free apis<br /> 
+        -get stats <br />
+        - help and tips section.
+        <br />
+        -get only important charts and mcaps volumes deltas etc, noise canceling mode.
+        
+        </h3>
+
+      </h2>
+    </div>
+  );
+}
